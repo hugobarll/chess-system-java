e6480b290580b111f53760957931d3276225418d
Victor Hugo <victorhugo.prog@gmail.com>
2026-09-10 00:39:04 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -19,4 +19,12 @@ public class Board {
     public int getColumns() {
         return columns;
     }
+
+    public Piece piece (int rows, int columns) {
+        return pieces[rows][columns];
+    }
+
+    public Piece piece (Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
 }
